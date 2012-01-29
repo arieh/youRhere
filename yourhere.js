f099e52e43ec1eed66ccecee86a01207c0a75fd9
Arieh Glazer <arieh.glazer@gmail.com>
2012-01-29 23:00:14 +0200

    working demo

--- yourhere.js
+++ yourhere.js
@@ -1,18 +1,18 @@
 /**************************************************************
-	* youRhere v1.0 - Vanilla Plugin
-	* Copyright 2012, Arieh Glazer
-	* http://arieh.co.il
-	* Dual licensed under the MIT or GPL Version 2 licenses.		
-	* Enjoy :)
+    * youRhere v1.0 - Vanilla Plugin
+    * Copyright 2012, Arieh Glazer
+    * http://arieh.co.il
+    * Dual licensed under the MIT or GPL Version 2 licenses.
+    * Enjoy :)
 ***************************************************************/
 
 (function(ns,doc) {
     function intgr(str){
-        return parseInt(str,10);    
+        return parseInt(str,10);
     }
 
     function has(obj,prop){
-        return Object.prototype.hasOwnProperty.call(obj,prop);    
+        return Object.prototype.hasOwnProperty.call(obj,prop);
     }
 
     function merge(obj, params){
@@ -22,30 +22,30 @@
         }
     }
 
-    function camleCase(str){
+    function camelCase(str){
         return str.replace(/\-(\w)/g, function (strMatch, p1) {
             return p1.toUpperCase();
-        });                 
+        });
     }
 
 
     function setStyles(el, styles, value){
         var name;
- 
-        function setStyle(name,value){ 
+
+        function setStyle(name,value){
             name = camelCase(name);
-            el.style[name] = styles[name];        
-        }        
+            el.style[name] = styles[name];
+        }
 
         if (value) return setStyle(styles, value);
 
-        for (name in styles) if has(styles,name){
+        for (name in styles) if (has(styles,name)){
             setStyle(name,styles[name]);
         }
     }
- 
+
     var getStyle, addEvent, bind, indexOf;
-    
+
     getStyle = (function() {
         return function (element, cssRule){
             var str = "";
@@ -60,28 +60,28 @@
         };
     })();
 
-    addEvent = 'addEventListener' in document 
+    addEvent = 'addEventListener' in document
         ? function(el, type, fn, phase){
-            el.addEventListener(type,fn,phase || false);    
+            el.addEventListener(type,fn,phase || false);
         }
         : function(el,type,fn,phase){
-            el.attachEvent('on'+type,fn,phase);        
+            el.attachEvent('on'+type,fn,phase);
         };
 
-    bind = 'bind' in addEvent 
+    bind = 'bind' in addEvent
         ? function(scope, fn){
-            if (typeof fn == 'string') return scope[fn].bind(scope);    
-            
+            if (typeof fn == 'string') return scope[fn].bind(scope);
+
             return fn.bind(scope);
         }
         : function(scope, fn){
-            if (typeof fn == 'string') fn = scope[fn]; 
+            if (typeof fn == 'string') fn = scope[fn];
             return function(){
                 fn.apply(scope, arguments);
-            }    
+            };
         };
 
-    indexOf = 'indexOf' in [] 
+    indexOf = 'indexOf' in []
         ? function(arr,needle){
             return arr.indexOf(needle);
         }
@@ -98,6 +98,8 @@
         this.options = YouAreHere.defaultOptions;
         merge(this.options,options);
 
+        this.elements = {};
+                         
         this.bound = {
             click : bind(this,'click'),
             move  : bind(this,'move'),
@@ -107,16 +109,16 @@
         this.setStyles();
         this.generate();
         this.attach();
-    };
+    }
 
     YouAreHere.prototype = {
         constructor : YouAreHere,
-        setStyles : function(){ 
+        setStyles : function(){
             var child = this.target.firstChild;
 
             this.styles = {
-                lineHeight : getStyle(this.target,'fontSize'),
-                zIndex : getStyle(this.target,'zIndex')
+                lineHeight : getStyle(this.target,'font-size'),
+                zIndex : getStyle(this.target,'z-index')
             };
 
             if (this.styles.zIndex == 'auto') this.styles.zIndex = 10;
@@ -124,56 +126,57 @@
             if (getStyle(this.target,'position')=='static') setStyles(this.target,'position','relative');
 
             while (child){
-                if (child.nodeType ==1){
+                if (child.nodeType==1){
                     setStyles(child,{
                         'z-index' : this.styles.zIndex + 2,
                         'position' : 'relative'
                     });
-                }    
+                }
+                child = child.nextSibling;
             }
         },
 
         generate : function(){
             this.elements.marker = document.createElement('div');
             this.elements.marker.className = 'yourhere-marker';
-            setStyles(this.elements.marker,{
-				position: 'absolute',
-				zIndex: this.styles.zIndex + 3,
-				width: this.options.markerWidth,
-				cursor: 'pointer',
-				background : this.options.markerBackground,
-				height: this.styles.lineHeight + 'px' 
-            });  
-			
+
             this.elements.markerLine = document.createElement('div');
             this.elements.markerLine.className = 'yourhere-marker line';
-
-			setStyles(this.elements.markerLine,{
-				position: 'absolute',
-				zIndex: this.styles.zIndex + 1,
-				backgroundColor: this.options.markerLineBackground,
-				opacity: this.options.markerBackgroundOpacity,
-				height: this.styles.lineHeight + 'px'		
-			});	 
 
             this.elements.tempMarker = document.createElement('div');
             this.elements.tempMarker.className = 'yourhere-marker temp';
 
-            setStyles(this.elements.tempMarker,{
-				position: 'absolute',
-				zIndex: this.styles.zIndex + 1,
-				width: this.options.markerWidth,
-				height: this.styles.lineHeight + 'px',				
-				top: 0, 
-				background: this.options.tempMarkerBackground
-			});
-            setStyles(this.elements.tempMarker, this.options.markerDirection, (-this.options.markerWidth) +'px');
-            setStyles(this.elements.marker, this.options.markerDirection, (-this.options.markerWidth) +'px');
-            setStyles(this.elements.markerLine, this.options.markerDirection, (-this.options.markerWidth) +'px');
-
             this.elements.style = document.createElement('style');
             this.elements.style.type = 'text/css';
-            this.elements.style.appendChild( document.createTextNode(YouAreHere.css) );
+            this.elements.style.appendChild( document.createTextNode(this.options.parentClass + ' ' +YouAreHere.css) );
+
+            setStyles(this.elements.marker, {
+                zIndex: this.styles.zIndex + 3,
+                width: this.options.markerWidth,
+                cursor: 'pointer',
+                background : this.options.markerBackground,
+                height: this.styles.lineHeight + 'px'
+            });
+
+            setStyles(this.elements.markerLine, {
+                zIndex: this.styles.zIndex + 1,
+                backgroundColor: this.options.markerLineBackground,
+                opacity: this.options.markerBackgroundOpacity,
+                height: this.styles.lineHeight + 'px'
+            });
+
+            setStyles(this.elements.tempMarker, {
+                zIndex: this.styles.zIndex + 1,
+                width: this.options.markerWidth,
+                height: this.styles.lineHeight + 'px',
+                top: 0,
+                background: this.options.tempMarkerBackground
+            });
+
+            this.elements.style.appendChild( document.createTextNode(
+                ".yourhere-marker { "+this.options.markerDirection +": -"+(intgr(this.options.markerWidth))+"px; }"
+            ));
+
             document.getElementsByTagName('head')[0].appendChild(this.elements.style);
 
             this.target.appendChild(this.elements.marker);
@@ -182,8 +185,6 @@
         },
 
         attach : function(){
-			var boxOffset = box.offset().top;
-
             addEvent(this.target, 'click', this.bound.click);
 
             addEvent(this.target, 'mousemove', this.bound.move);
@@ -194,47 +195,45 @@
         click : function(e){
             var el = e.target,
                 elements = this.options.supportedElements,
-                name = e.nodeName.toLowerCase(),
+                name = el.nodeName.toLowerCase(),
                 targetOffset = this.target.offsetTop,
-                lineHeight = intgr(getStyle(el,'lineHight')),
-                fontSize = intgr(getStyle(el,'fontSize')),
+                lineHeight = intgr(getStyle(el,'line-hight')),
+                fontSize = intgr(getStyle(el,'font-size')),
                 offset,
                 z,
-                e,
+                x,
                 y
             ;
-            
+
             if (indexOf(elements, name) == -1) return;
 
-            if (fontSize > lineHeight) lineHeight = fontSize;
+            if (!lineHeight || fontSize > lineHeight) lineHeight = fontSize;
             offset = el.offsetTop;
             z = e.pageY - offset;
-            e = Math.floor(z / lineHeight);
-            y = offset - targetOffset + (e * lineHeight);
+            x = Math.floor(z / lineHeight);
+            y = offset - targetOffset + (x * lineHeight);
 
             this.markerCreator(y, lineHeight);
         },
 
-        move : function(e){ 
+        move : function(e){
             var el = e.target,
                 elements = this.options.supportedElements,
-                name = e.nodeName.toLowerCase(),
+                name = el.nodeName.toLowerCase(),
                 targetOffset = this.target.offsetTop,
-                lineHeight = intgr(getStyle(el,'lineHight')),
-                fontSize = intgr(getStyle(el,'fontSize')),
+                lineHeight = intgr(getStyle(el,'line-hight')),
+                fontSize = intgr(getStyle(el,'font-size')),
                 offset,
-                z,
-                e,
                 y
             ;
-            
-            if (indexOf(elements, name) == -1) return; 
 
-            if (fontSize > lineHeight) lineHeight = fontSize;
-            
+            if (indexOf(elements, name) == -1) return;
+
+            if (!lineHeight || fontSize > lineHeight) lineHeight = fontSize;
+
             y = e.pageY - targetOffset - lineHeight/2;
 
-			if(e.pageY < targetOffset + lineHeight / 2 || e.pageY > this.target.offsetHeight + targetOffset) return;
+            if(e.pageY < targetOffset + lineHeight / 2 || e.pageY > this.target.offsetHeight + targetOffset) return;
 
             setStyles(this.elements.tempMarker,{
                 top : y + 'px',
@@ -243,48 +242,51 @@
         },
 
         dblClick : function(e){
-            setStyle(this.elements.marker,'opacity',0);
-            setStyle(this.elements.markerLine,'opacity',0);
+            setStyles(this.elements.marker,'opacity',0);
+            setStyles(this.elements.markerLine,'opacity',0);
         },
-		 
-		markerCreator: function(y, lineHeight) {
-			box.find('.yourhere-marker, .yourhere-markerline').fadeOut('fast');
-			
+
+        markerCreator: function(y, lineHeight) {
             setStyles(this.elements.marker,{
                 height : lineHeight + 'px',
                 top : y+'px'
             });
-			
+
             setStyles(this.elements.markerLine,{
                 width : (this.target.offsetWidth + this.options.markerWidth) + 'px',
                 height : lineHeight + 'px',
                 top : y+'px'
             });
-		}
+        }
     };
 
     YouAreHere.defaultOptions = {
-		markerDirection: 'left',
-		tempMarkerBackground: '#b7b7b7',
-		markerLineBackground: '#FFF82A',
-		markerBackgroundOpacity: '0.7',
-		markerBackground: '#000',
-		markerWidth: '5px',
-		supportedElements: 'h1 h2 h3 h4 h5 h6 p ul li'.split(' ')
-	};
+        markerDirection: 'left',
+        tempMarkerBackground: '#b7b7b7',
+        markerLineBackground: '#FFF82A',
+        markerBackgroundOpacity: '0.7',
+        markerBackground: '#000',
+        markerWidth: '5px',
+        parentClass: '',
+        supportedElements: 'h1 h2 h3 h4 h5 h6 p ul li'.split(' ')
+    };
 
-    YouAreHere.css = 
+    YouAreHere.css =
         ".yourhere-marker {"
+            +"position: absolute;"
+
             +"-webkit-transition-property: opacity, top;"
-            +"-moz-transition-property: opacity, top;"
-            +"transition-property: opacity, top;" 
+               +"-moz-transition-property: opacity, top;"
+                    +"transition-property: opacity, top;"
+
             +"-webkit-transition-duration: 1s,1s;"
-            +"-moz-transition-duration: 1s,1s;"
-            +"transition-duration: 1s,1s;" 
-            +"-webkit-transition-timing-function:ease-in,ease-in"
-            +"-moz-transition-timing-function:ease-in,ease-in"
-            +"transition-timing-function:ease-in,ease-in"
+               +"-moz-transition-duration: 1s,1s;"
+                    +"transition-duration: 1s,1s;"
+
+            +"-webkit-transition-timing-function:ease-in,ease-in;"
+               +"-moz-transition-timing-function:ease-in,ease-in;"
+                    +"transition-timing-function:ease-in,ease-in;"
         +"}";
- 
+
     this.YouAreHere = YouAreHere;
 }).call(this,document);
